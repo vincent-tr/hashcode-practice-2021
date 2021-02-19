262c4a808a3f77c00900890057079be48977e38f
Volcomix <Volcomix@users.noreply.github.com>
2021-02-19 09:01:49 +0100

    Refactor dataset entities

--- src/dataset.test.ts
+++ src/dataset.test.ts
@@ -1,9 +1,33 @@
 import { assertEquals } from "https://deno.land/std@0.87.0/testing/asserts.ts";
-import { Dataset, readDataset } from "./dataset.ts";
+import {
+  Dataset,
+  parsePizza,
+  parseTeams,
+  Pizza,
+  readDataset,
+  Team,
+} from "./dataset.ts";
+
+Deno.test("parses teams", () => {
+  const teams = parseTeams("5 1 2 3");
+  assertEquals(teams, [
+    { peopleCount: 2, teamCount: 1 },
+    { peopleCount: 3, teamCount: 2 },
+    { peopleCount: 4, teamCount: 3 },
+  ] as Team[]);
+});
+
+Deno.test("parses pizza", () => {
+  const pizza = parsePizza("3 onion pepper olive", 0);
+  assertEquals(pizza, {
+    id: 0,
+    ingredients: ["onion", "pepper", "olive"],
+  } as Pizza);
+});
 
 Deno.test("reads dataset", async () => {
   const dataset = await readDataset("input/a_example");
-  assertEquals<Dataset>(dataset, {
+  assertEquals(dataset, {
     teams: [
       { peopleCount: 2, teamCount: 1 },
       { peopleCount: 3, teamCount: 2 },
@@ -16,5 +40,5 @@ Deno.test("reads dataset", async () => {
       { id: 3, ingredients: ["tomato", "mushroom", "basil"] },
       { id: 4, ingredients: ["chicken", "basil"] },
     ],
-  });
+  } as Dataset);
 });
--- src/dataset.ts
+++ src/dataset.ts
@@ -1,9 +1,16 @@
-import { parsePizza, Pizza } from "./pizza.ts";
-import { parseTeams, Team } from "./team.ts";
-
 export type Dataset = {
   teams: Team[];
   pizzas: Pizza[];
+};
+
+export type Team = {
+  peopleCount: number;
+  teamCount: number;
+};
+
+export type Pizza = {
+  id: number;
+  ingredients: string[];
 };
 
 export async function readDataset(inputFilePath: string): Promise<Dataset> {
@@ -16,5 +23,22 @@ export async function readDataset(inputFilePath: string): Promise<Dataset> {
   return {
     teams: parseTeams(teamsLine),
     pizzas: pizzasLines.map(parsePizza),
+  };
+}
+
+export function parseTeams(line: string): Team[] {
+  return line.split(" ")
+    .slice(1)
+    .map(Number)
+    .map((teamCount, i) => ({
+      peopleCount: i + 2,
+      teamCount,
+    }));
+}
+
+export function parsePizza(line: string, pizzaId: number): Pizza {
+  return {
+    id: pizzaId,
+    ingredients: line.split(" ").slice(1),
   };
 }
