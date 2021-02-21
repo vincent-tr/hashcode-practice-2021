8ee186dd095d9a573830933710033c13c87507b7
Volcomix <Volcomix@users.noreply.github.com>
2021-02-21 19:24:34 +0100

    Init linked list for pizzas

--- src/solvers/greedy.ts
+++ src/solvers/greedy.ts
@@ -8,7 +8,7 @@ import { SolverProgress } from "../helpers/solver.ts";
 import { Submission } from "../submission.ts";
 
 self.onmessage = async ({ data: dataset }: MessageEvent<Dataset>) => {
-  const { name, teams, pizzas } = dataset;
+  const { name, teams } = dataset;
   const maxPizzaCount = Math.min(
     countTotalPeople(dataset),
     countTotalPizzas(dataset),
@@ -16,36 +16,23 @@ self.onmessage = async ({ data: dataset }: MessageEvent<Dataset>) => {
   const submission: Submission = { name, deliveries: [] };
   const progress: SolverProgress = { name, progress: 0, total: maxPizzaCount };
   self.postMessage(progress);
-  teams.sort((a, b) => b.personCount - a.personCount);
-  pizzas.sort((a, b) => b.ingredients.length - a.ingredients.length);
-  let pizzaIdx = 0;
+  teams.reverse();
+  let pizzas = toLinkedList(dataset.pizzas);
   for (const { personCount, teamCount } of teams) {
-    for (
-      let teamIdx = 0;
-      teamIdx < teamCount && pizzaIdx < pizzas.length &&
-      personCount <= pizzas.length - pizzaIdx;
-      teamIdx++
-    ) {
-      // TODO Monte-Carlo on teams order
+    // TODO Monte-Carlo on teams order
+    for (let teamIdx = 0; teamIdx < teamCount && pizzas; teamIdx++) {
       // Always ends with 3-person + 2-person deliveries
       if (
-        pizzas.length - pizzaIdx === 5 &&
-        personCount === 4
+        dataset.pizzas.length - progress.progress === 5 && personCount === 4
       ) {
         break;
       }
       const pizzasToDeliver: Pizza[] = [];
       const ingredients = new Set<string>();
-      for (
-        let personIdx = 0;
-        personIdx < personCount && pizzaIdx < pizzas.length;
-        personIdx++
-      ) {
-        // Uncomment next line to simulate long processing
-        // await new Promise((resolve) => setTimeout(resolve));
-
-        const pizza = pizzas[pizzaIdx++];
+      for (let personIdx = 0; personIdx < personCount && pizzas; personIdx++) {
+        const pizza = pizzas.pizza;
         pizzasToDeliver.push(pizza);
+        pizzas = pizzas.next;
         for (const ingredient of pizza.ingredients) {
           ingredients.add(ingredient);
         }
@@ -55,12 +42,18 @@ self.onmessage = async ({ data: dataset }: MessageEvent<Dataset>) => {
           score: ingredients.size * ingredients.size,
           pizzas: pizzasToDeliver,
         });
+        if (teamIdx % 10 === 0) {
+          progress.progress += pizzasToDeliver.length;
+          self.postMessage(progress);
+        }
       } else {
-        pizzaIdx -= pizzasToDeliver.length;
-      }
-      if (teamIdx % 10 === 0) {
-        progress.progress = pizzaIdx;
-        self.postMessage(progress);
+        while (pizzasToDeliver.length) {
+          pizzas = {
+            pizza: pizzasToDeliver.pop()!,
+            next: pizzas,
+          };
+        }
+        break;
       }
     }
   }
@@ -69,3 +62,19 @@ self.onmessage = async ({ data: dataset }: MessageEvent<Dataset>) => {
   self.postMessage(submission);
   self.close();
 };
+
+type PizzaNode = {
+  pizza: Pizza;
+  next: PizzaNode | null;
+};
+
+export function toLinkedList(pizzas: Pizza[]): PizzaNode | null {
+  return pizzas
+    // TODO Invert the sort and remove the reverse once greedy works
+    .sort((a, b) => b.ingredients.length - a.ingredients.length)
+    .reverse() // Just to get same result as first implem
+    .reduce(
+      (pizzaNode, pizza) => ({ pizza, next: pizzaNode }),
+      null as PizzaNode | null,
+    );
+}
